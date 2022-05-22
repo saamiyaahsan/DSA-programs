09aaf94a3c62e4390d80f99a925d3225e3785900
Saamiya Ahsan <89975613+saamiyaahsan@users.noreply.github.com>
2022-05-22 16:45:39 +0530

    Create Lower Triangular Matrix in C using row major.c

---
+++ Lower Triangular Matrix in C using row major.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct Matrix
+{
+ int *A;   
+ int n;    
+};
+void set(struct Matrix *m,int i,int j,int x)
+{  if(i>=j)
+   m->A[i*(i-1)/2 + j-1] = x;
+    
+}
+int get(struct Matrix m,int i,int j)
+{   if(i>=j)
+    return m.A[i*(i-1)/2 + j-1];   
+    else
+    return 0;
+}
+void display(struct Matrix m)
+{int i,j;
+ for(i=1;i<=m.n;i++)
+  { 
+      for(j=1;j<=m.n;j++)
+      {
+        if(i>=j)
+        printf("%d\t",m.A[i*(i-1)/2 + j-1]);
+        else
+        printf("0\t");
+      }
+     printf("\n"); 
+  }
+    
+}
+int main()
+{   int i,j,x;
+    struct Matrix m;
+    printf("Enter dimensions");
+    scanf("%d",&m.n);
+    m.A = (int *)malloc(m.n*(m.n+1)/2*sizeof(int));
+    printf("Enter all elements");
+    for(i=1;i<=m.n;i++)
+    {
+      for(j=1;j<=m.n;j++)
+      {
+         scanf("%d",&x); 
+         set(&m,i,j,x); 
+      }  
+        
+    }
+    printf("\n\n");
+    display(m);
+    return 0;
+}
+
