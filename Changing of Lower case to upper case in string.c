df7469a13cacc4ddbd989590193d0847bcafb9ec
Saamiya Ahsan <89975613+saamiyaahsan@users.noreply.github.com>
2022-05-22 16:33:14 +0530

    Create Changing of Lower case to upper case in string.c

---
+++ Changing of Lower case to upper case in string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{   char A[] = "welcome";
+    int i;
+    for(i=0;A[i]!='\0';i++)
+    {
+        A[i] = A[i] - 32;
+    }
+    printf("%s",A);
+    return 0;
+}
+
