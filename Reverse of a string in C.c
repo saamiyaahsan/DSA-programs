c5033d71d12e4838030814ded2cca98b93d92f17
Saamiya Ahsan <89975613+saamiyaahsan@users.noreply.github.com>
2022-05-22 16:38:20 +0530

    Create Reverse of a string in C.c

---
+++ Reverse of a string in C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{   char A[] = "Quest";
+    char B[7];
+    int i,j;
+    for(i=0;A[i]!='\0';i++)
+    {
+        
+    }
+    i=i-1;
+    for(j=0;i>=0;i--,j++)
+    {
+      B[j] = A[i];
+    }
+    B[j] = '\0';
+    printf("%s",B);
+    return 0;
+}
+
